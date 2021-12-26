fb79ba3cd668a3df207896b78525ff0840fe264b
Sandeep Ramgolam <sandeep@ramgolam.com>
2021-12-26 14:05:33 +0400

    added useGoogleSheet composable

---
+++ src/logic/useGoogleSheet.ts
@@ -0,0 +1,63 @@
+/* 
+
+//INPUT
+API_KEY
+SHEET_ID
+SHEET_NAME(optional)
+
+// Returns
+headers
+items(formatter as { header: value }
+loading ref
+
+*/
+
+import { ref, reactive, Ref } from "vue"
+
+interface I {
+  [key: string]: any
+}
+
+export default (API_KEY: string = 'xxx', SHEET_ID: string = 'yyy', SHEET_NAME: string = 'Sheet1') => {
+
+  const loading: Ref<boolean> = ref(true)
+  let headers: string[] = reactive([])
+  let items = reactive([]) as I[]
+
+
+  function formatter(input: string[]) {
+    const entry = input
+
+    let records = entry.splice(1, entry.length)
+    let columns = entry[0].length
+
+    for (let i = 0; i < columns; i++) {
+      headers.push(entry[0][i])
+    }
+
+    for (let i = 0; i < records.length; i++) {
+      const item = {} as I
+      for (let j = 0; j < headers.length; j++) {
+        item[headers[j]] = records[i][j]
+      }
+      items.push(item)
+    }
+  }
+
+
+  fetch(`https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_NAME}?key=${API_KEY}`)
+    .then(res => res.json())
+    .then(res => {
+      loading.value = false
+      formatter(res.values)
+    }).catch(err => {
+      console.log(err)
+    })
+
+  return {
+    headers: readonly(headers),
+    items: readonly(items),
+    loading
+  }
+
+}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,54 +1,23 @@
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user'
 
-const user = useUserStore()
-const name = ref(user.savedName)
+import useSheet from '@/logic/useGoogleSheet'
+const API_KEY = 'AIzaSyClsialqDzA0V4_fuxURgwjTEaCxU788xs'
+const SHEET_ID = '1i1_6IOA4wdm5Cl-hXRf46Dkwqz5bMcN3KdFcaGahbV0'
+const { headers, items, loading } = useSheet(API_KEY, SHEET_ID);
 
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
-}
 </script>
 
 <template>
-  <div class="grid place-items-center">
-    <Branding>
-      <p>
-        <a
-          rel="noreferrer"
-          href="https://github.com/MrSunshyne/ti-vitesse"
-          target="_blank"
-        >Ti-Vitesse</a>
-      </p>
-    </Branding>
-    <p>
-      <em class="text-sm opacity-75">Opinionated vite starter</em>
-    </p>
-
-    <div class="py-4" />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="Whats your name"
-      aria-label="intro.whats-your-name"
-      type="text"
-      autocomplete="false"
-      class="outline-none active:outline-none px-5 py-4 w-[250px] text-center bg-transparent rounded border-gray-200 dark:border-gray-700"
-      @keydown.enter="go"
-    />
-    <label class="hidden" for="input">What's your name</label>
-
-    <div>
-      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
-        go
-      </button>
+  <div class="prose">
+    <pre v-if="loading">loading...</pre>
+    <div v-else>
+      <pre>{{ headers }}</pre>
+      <pre>{{ items }}</pre>
     </div>
   </div>
-</template>
+</template>   
 
 <route lang="yaml">
 meta:
-  layout: home
+  layout: default
 </route>
