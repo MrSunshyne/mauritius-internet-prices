13aeb9d13b046bf1286b551b761a22039af3e3c6
Sandeep Ramgolam <sandeep@ramgolam.com>
2021-12-27 09:52:22 +0400

    added operators

---
+++ src/components/Operators.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="space-y-2">
+    <h2 class="text-xl font-medium">Operators</h2>
+    <div class="flex gap-8">
+      <div
+        class="text-2xl font-bold shadow-md px-8 py-4 rounded-lg bg-white hover:shadow-lg transition-all"
+        v-for="operator in operators"
+      >{{ operator }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">import { PropType } from 'vue';
+interface Row {
+  ["Package Name"]: string
+  ["Provider"]: string
+  ["Duration(days)"]: string
+  ["Volume (GB)"]: string
+  ["Volume per day"]: string
+  ["Cost(Rs)"]: string
+  ["Cost(Rs) per day"]: string
+  ["Cost per GB"]: string
+  ["Source"]: string
+}
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<Row[]>,
+    default: () => [],
+  },
+})
+
+const operators = computed(() => {
+  let results = new Set();
+  let items = props.items
+  for (let index = 0; index < items.length; index++) {
+    results.add(items[index]["Provider"])
+  }
+  console.log(results);
+  return results
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,8 @@
 
 import useSheet from '@/logic/useGoogleSheet'
 import TableComparison from '@/components/TableComparison.vue';
+import Operators from '@/components/Operators.vue';
+import FrequentlyAskedQuestions from '@/components/FrequentlyAskedQuestions.vue';
 const API_KEY = 'AIzaSyClsialqDzA0V4_fuxURgwjTEaCxU788xs'
 const SHEET_ID = '1i1_6IOA4wdm5Cl-hXRf46Dkwqz5bMcN3KdFcaGahbV0'
 const { headers, items, loading } = useSheet(API_KEY, SHEET_ID);
@@ -14,7 +16,9 @@ const { headers, items, loading } = useSheet(API_KEY, SHEET_ID);
   <div v-else>
     <!-- {{ headers }} -->
     <div class="flex flex-col gap-8 py-8">
+      <Operators :items="items" />
       <TableComparison :headers="headers" :rows="items"></TableComparison>
+      <FrequentlyAskedQuestions />
     </div>
   </div>
 </template>   
